d6936c80d29ea13ce1613e35c26796d1e0f72053
abdumbo99 <razoq1999@gmail.com>
2023-04-10 18:48:23 +0300

    code for model loading #12

--- Vanny/pi4/src/main.py
+++ Vanny/pi4/src/main.py
@@ -1,3 +1,57 @@
+# Import the necessary libraries
+import picamera
+import numpy as np
+import tensorflow as tf
 
-if __name__ == '__main__':
-    pass
+# Set the resolution and framerate of the camera
+camera = picamera.PiCamera(resolution=(640, 480), framerate=30)
+
+# Load the TensorFlow Lite model
+interpreter = tf.lite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
+
+# Get the input and output tensors
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+# Define a function to preprocess the image
+def preprocess(image):
+    # Convert the image to a numpy array
+    image = np.asarray(image)
+    # Normalize the pixel values to be between 0 and 1
+    image = image / 255.0
+    # Resize the image to the input shape of the model
+    image = tf.image.resize(image, (input_details[0]['shape'][1], input_details[0]['shape'][2]))
+    # Add a batch dimension to the image
+    image = np.expand_dims(image, axis=0)
+    # Convert the image to the input data type of the model
+    image = np.array(image, dtype=input_details[0]['dtype'])
+    return image
+
+# Define a function to detect objects in the image
+def detect_objects(image):
+    # Preprocess the image
+    image = preprocess(image)
+    # Set the input tensor to the preprocessed image
+    interpreter.set_tensor(input_details[0]['index'], image)
+    # Run the model
+    interpreter.invoke()
+    # Get the output tensor
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    # Return the detected objects
+    return output_data[0]
+
+# Start the camera preview
+camera.start_preview()
+
+# Continuously capture images and detect objects
+while True:
+    # Capture an image
+    image = camera.capture()
+    # Detect objects in the image
+    objects = detect_objects(image)
+    # Print the detected objects
+    print(objects)
+
+# Stop the camera preview
+camera.stop_preview()
