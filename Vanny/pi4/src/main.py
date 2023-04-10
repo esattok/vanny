5d63c8f4a5d852c4e02afd6cd15f1fb988d1146a
abdumbo99 <razoq1999@gmail.com>
2023-04-10 19:26:11 +0300

    more fixes

--- Vanny/pi4/src/main.py
+++ Vanny/pi4/src/main.py
@@ -5,62 +5,81 @@ import tflite_runtime.interpreter as tflite_interpreter
 from io import BytesIO
 import cv2
 
-# Load the TFLite model
-interpreter = tflite_interpreter.Interpreter(model_path='detect.tflite')
+# Set the resolution and framerate of the camera
+camera = picamera.PiCamera(resolution=(640, 480), framerate=30)
+
+# Load the TensorFlow Lite model
+interpreter = tflite_interpreter.Interpreter(model_path="model.tflite")
 interpreter.allocate_tensors()
 
-# Get the input and output details of the model
+# Get the input and output tensors
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
-height = input_details[0]['shape'][1]
-width = input_details[0]['shape'][2]
 
-# Initialize the PiCamera and set its resolution
-camera = picamera.PiCamera()
-camera.resolution = (640, 480)
+# Define a function to preprocess the image
+def preprocess(image):
+    # Convert the image to a numpy array
+    image = np.asarray(image)
+    # Normalize the pixel values to be between 0 and 1
+    image = image / 255.0
+    # Resize the image to the input shape of the model
+    image = np.resize(image, (input_details[0]['shape'][1], input_details[0]['shape'][2], input_details[0]['shape'][3]))
+    # Add a batch dimension to the image
+    image = np.expand_dims(image, axis=0)
+    # Convert the image to the input data type of the model
+    image = np.array(image, dtype=input_details[0]['dtype'])
+    return image
 
-# Start the PiCamera preview
+# Define a function to detect objects in the image
+def detect_objects(image):
+    # Preprocess the image
+    image = preprocess(image)
+    # Set the input tensor to the preprocessed image
+    interpreter.set_tensor(input_details[0]['index'], image)
+    # Run the model
+    interpreter.invoke()
+    # Get the output tensor
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    # Return the detected objects
+    return output_data[0]
+
+# Define a function to draw boxes around the detected objects
+def draw_boxes(image, objects):
+    # Get the dimensions of the image
+    height, width, _ = image.shape
+    # Loop over the detected objects
+    for obj in objects:
+        # Get the coordinates of the box
+        ymin = int(obj['bounding_box']['ymin'] * height)
+        xmin = int(obj['bounding_box']['xmin'] * width)
+        ymax = int(obj['bounding_box']['ymax'] * height)
+        xmax = int(obj['bounding_box']['xmax'] * width)
+        # Draw the box on the image
+        cv2.rectangle(image, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+    # Return the image with the boxes drawn
+    return image
+
+# Start the camera preview
 camera.start_preview()
 
-# Initialize the stream for the PiCamera output
-stream = BytesIO()
-
-# Continuously capture frames from the PiCamera
-for _ in camera.capture_continuous(stream, format='jpeg', use_video_port=True):
-    # Convert the captured image to a numpy array
-    frame = np.frombuffer(stream.getvalue(), dtype=np.uint8)
-    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-
-    # Preprocess the image
-    resized = cv2.resize(frame, (width, height))
-    input_data = np.expand_dims(resized, axis=0)
-    input_data = (np.float32(input_data) - 127.5) / 127.5
-
-    # Run inference on the input image
-    interpreter.set_tensor(input_details[0]['index'], input_data)
-    interpreter.invoke()
-    output_data = interpreter.get_tensor(output_details[0]['index'])
-
-    # Get the bounding boxes for the detected objects
-    boxes = output_data[0, :, :]
-
-    # Filter out low-confidence detections
-    detections = [((int(box[1] * frame.shape[1]), int(box[0] * frame.shape[0])),
-                   (int(box[3] * frame.shape[1]), int(box[2] * frame.shape[0]))) for box in boxes if box[4] > 0.6]
-
-    # Draw the bounding boxes for the detected objects
-    for detection in detections:
-        box_start, box_end = detection
-        cv2.rectangle(frame, box_start, box_end, (0, 255, 0), 2)
-
-    # Display the processed frame
-    cv2.imshow('Object Detection', frame)
+# Continuously capture images and detect objects
+while True:
+    # Create a BytesIO object to store the captured image
+    image_stream = BytesIO()
+    # Capture an image and store it in the BytesIO object
+    camera.capture(image_stream, format='jpeg')
+    # Convert the image data in the BytesIO object to a numpy array
+    image = np.frombuffer(image_stream.getvalue(), dtype=np.uint8)
+    # Decode the image as a JPEG image
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    # Detect objects in the image
+    objects = detect_objects(image)
+    # Draw boxes around the detected objects
+    image_with_boxes = draw_boxes(image, objects)
+    # Show the image with the boxes
+    cv2.imshow('Object Detection', image_with_boxes)
+    # Wait for a key press
     cv2.waitKey(1)
 
-    # Reset the stream for the next capture
-    stream.seek(0)
-    stream.truncate()
-
-# Clean up the resources
+# Stop the camera preview
 camera.stop_preview()
-cv2.destroyAllWindows()
