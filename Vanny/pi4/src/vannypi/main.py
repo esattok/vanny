d895c034c52482eb5a5ed45cbd18a65caadb1520
abdumbo99 <razoq1999@gmail.com>
2023-04-13 03:14:01 +0300

    separated the flow into their classes #12

--- Vanny/pi4/src/vannypi/main.py
+++ Vanny/pi4/src/vannypi/main.py
@@ -1,70 +1,7 @@
-# Copyright 2021 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""Main script to run the object detection routine."""
-
-
-import cv2
-import numpy as np
-from tflite_support.task import processor
-
-_MARGIN = 10  # pixels
-_ROW_SIZE = 10  # pixels
-_FONT_SIZE = 1
-_FONT_THICKNESS = 1
-_TEXT_COLOR = (0, 0, 255)  # red
-
-
-def visualize(
-    image: np.ndarray,
-    detection_result: processor.DetectionResult,
-) -> np.ndarray:
-  """Draws bounding boxes on the input image and return it.
-
-  Args:
-    image: The input RGB image.
-    detection_result: The list of all "Detection" entities to be visualize.
-
-  Returns:
-    Image with bounding boxes.
-  """
-  for detection in detection_result.detections:
-    # Draw bounding_box
-    bbox = detection.bounding_box
-    start_point = bbox.origin_x, bbox.origin_y
-    end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
-    cv2.rectangle(image, start_point, end_point, _TEXT_COLOR, 3)
-
-    # Draw label and score
-    category = detection.categories[0]
-    category_name = category.category_name
-    probability = round(category.score, 2)
-    result_text = category_name + ' (' + str(probability) + ')'
-    text_location = (_MARGIN + bbox.origin_x,
-                     _MARGIN + _ROW_SIZE + bbox.origin_y)
-    cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-                _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS)
-
-  return image
-
 import argparse
-import sys
-import time
 
-import cv2
-from tflite_support.task import core
-from tflite_support.task import processor
-from tflite_support.task import vision
+from vannypi.inputanalysis.environmentanalyzer.object_identifier import ObjectsIdentifier
+from vannypi.inputmanagement.cameramanager.camera import Camera
 
 
 def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
@@ -79,78 +16,12 @@ def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
       num_threads: The number of CPU threads to run the model.
       enable_edgetpu: True/False whether the model is a EdgeTPU model.
     """
-
-    # Variables to calculate FPS
-    counter, fps = 0, 0
-    start_time = time.time()
-
-    # Start capturing video input from the camera
-    cap = cv2.VideoCapture(camera_id)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-
-    # Visualization parameters
-    row_size = 20  # pixels
-    left_margin = 24  # pixels
-    text_color = (0, 0, 255)  # red
-    font_size = 1
-    font_thickness = 1
-    fps_avg_frame_count = 10
-
-    # Initialize the object detection model
-    base_options = core.BaseOptions(
-        file_name=model, use_coral=enable_edgetpu, num_threads=num_threads)
-    detection_options = processor.DetectionOptions(
-        max_results=5, score_threshold=0.3)
-    options = vision.ObjectDetectorOptions(
-        base_options=base_options, detection_options=detection_options)
-    detector = vision.ObjectDetector.create_from_options(options)
-
-    # Continuously capture images from the camera and run inference
-    while cap.isOpened():
-        success, image = cap.read()
-        if not success:
-            sys.exit(
-                'ERROR: Unable to read from webcam. Please verify your webcam settings.'
-            )
-
-        counter += 1
-        image = cv2.flip(image, 1)
-
-        # Convert the image from BGR to RGB as required by the TFLite model.
-        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-        # Create a TensorImage object from the RGB image.
-        input_tensor = vision.TensorImage.create_from_array(rgb_image)
-
-        # Run object detection estimation using the model.
-        detection_result = detector.detect(input_tensor)
-
-        # Draw keypoints and edges on input image
-        image = visualize(image, detection_result)
-
-        # Calculate the FPS
-        if counter % fps_avg_frame_count == 0:
-            end_time = time.time()
-            fps = fps_avg_frame_count / (end_time - start_time)
-            start_time = time.time()
-
-        # Show the FPS
-        fps_text = 'FPS = {:.1f}'.format(fps)
-        text_location = (left_margin, row_size)
-        cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-                    font_size, text_color, font_thickness)
-
-        # Stop the program if the ESC key is pressed.
-        if cv2.waitKey(1) == 27:
-            break
-        cv2.imshow('object_detector', image)
-
-    cap.release()
-    cv2.destroyAllWindows()
+    model_options = ObjectsIdentifier.digest_models(model, num_threads, enable_edgetpu)
+    detector = ObjectsIdentifier.detect_objects(model_options)
+    Camera.run_camera(detector, height, width, camera_id)
 
 
-def main():
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument(
@@ -188,7 +59,3 @@ def main():
 
     run(args.model, int(args.cameraId), args.frameWidth, args.frameHeight,
         int(args.numThreads), bool(args.enableEdgeTPU))
-
-
-if __name__ == '__main__':
-    main()
