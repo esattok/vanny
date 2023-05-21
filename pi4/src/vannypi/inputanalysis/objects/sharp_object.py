c2bfb276fda051eead1f9c3b7288aae4dbdf4e12
abdumbo99 <razoq1999@gmail.com>
2023-05-22 02:38:41 +0300

    Keep track of sharp objects #139

--- pi4/src/vannypi/inputanalysis/objects/sharp_object.py
+++ pi4/src/vannypi/inputanalysis/objects/sharp_object.py
@@ -1,10 +1,23 @@
-from typing import List
-
-from vannypi.inputanalysis.objects.object import Object
+from typing import Dict, List
 
 
-class SharpObject(Object):
-    def __int__(self):
-        self.distance_from_toddler: float = 0.
-        self.dangerous: bool = True
-        self.size: List[float] = [0., 0.]
+class SharpObjects:
+    def __init__(self):
+        self._detected: Dict[str, int] = {'scissor': 0, 'knife': 0, 'pen': 0}
+        self.in_room: List[str] = []
+        self._THRESHOLD: int = 6
+
+    def update(self, names: List[str]):
+        for object_name in self._detected.keys():
+            if object_name in names:
+                if self._detected[object_name] < self._THRESHOLD:
+                    self._detected[object_name] += 1
+
+                if self._detected[object_name] == self._THRESHOLD and object_name not in self.in_room:
+                    self.in_room.append(object_name)
+
+            elif object_name not in names:
+                self._detected[object_name] -= 1
+
+                if self._detected[object_name] == 0:
+                    self.in_room.remove(object_name)
